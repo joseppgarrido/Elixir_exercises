de71e021d1c94e7acf3d3c5040bef378780505b5
José Garrido <jgarrido@godoycruz.gov.ar>
2022-03-15 13:17:45 -0300

    add prime_factor exercise

---
+++ prime_factor.ex
@@ -0,0 +1,18 @@
+defmodule PrimeFactors do
+  @doc """
+  Compute the prime factors for 'number'.
+
+  The prime factors are prime numbers that when multiplied give the desired
+  number.
+
+  The prime factors of 'number' will be ordered lowest to highest.
+  """
+  @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(number) do
+    factor(number, 2, [])
+  end
+
+  defp factor(1, _, acc), do: acc
+  defp factor(number, divisor, acc) when rem(number, divisor) == 0, do: factor(div(number, divisor), divisor, [divisor | acc])
+  defp factor(number, divisor, acc), do: factor(number, divisor + 1, acc)
+end
